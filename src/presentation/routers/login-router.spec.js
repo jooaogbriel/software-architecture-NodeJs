d8c6657d1dc3e2e79c1b6489dfebccdfb71d366f
João Gonçalves <joao_i0807@outlook.com>
2024-03-24 14:10:08 -0300

    ensure LoginRoute returns 500 if httpRequest is not provided and httpRequest has no body

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -2,7 +2,13 @@
 // --> EXEMPLO DE TESTE UNITÁRIO VERIFICANDO SE O STATUS CODE RETORNADO É 400 QUANDO NÃO É PASSADO O EMAIL NO CORPO DA REQUISIÇÃO 
 class LoginRouter {
     route(httpRequest){
-        if(!httpRequest.body.email){
+        if(!httpRequest || httpRequest.body === undefined){
+            return {
+                statusCode: 500
+            }
+        }
+        const { email, password } = httpRequest.body
+        if(!email || !password){
             return {
                 statusCode: 400
             }
@@ -20,5 +26,28 @@ describe('Login Router', () => {
         }
         const httpResponse = sut.route(httpRequest)
         expect(httpResponse.statusCode).toBe(400)
+    })
+
+    test('Should return 400 if no password is provided', () => {
+        const sut = new LoginRouter();
+        const httpRequest = {
+            body: { 
+                email: 'any_email'
+            }
+        }
+        const httpResponse = sut.route(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+    })
+
+    test('Should return 500 if no httRequest is provided', () => {
+        const sut = new LoginRouter();
+        const httpResponse = sut.route()
+        expect(httpResponse.statusCode).toBe(500)
+    })
+
+    test('Should return 500 if no httRequest has no body', () => {
+        const sut = new LoginRouter();
+        const httpResponse = sut.route({})
+        expect(httpResponse.statusCode).toBe(500)
     })
 })
